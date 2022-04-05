8f907eef47efa84dc51cd223e6fe40e2687c0c06
klltx <kostya.svetashov.01@gmail.com>
2022-04-06 00:17:59 +0400

    sign / encrypt / decrypt / verify

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("hello")
+const NodeRSA = require('node-rsa'),
+    fs = require('fs');
+
+// const message = fs.readFileSync("./data.txt", "utf8");
+
+const message = 'test message';
+
+const public_key_data = '-----BEGIN RSA PUBLIC KEY----- ... -----END RSA PUBLIC KEY-----';
+const private_key_data = '-----BEGIN RSA PRIVATE KEY----- ... -----END RSA PRIVATE KEY-----';
+
+const public_key = new NodeRSA({public_key_data}, 'pkcs1');
+const private_key = new NodeRSA({private_key_data}, 'pkcs1');
+
+// Sign and encrypt
+const sign = private_key.sign(message, 'base64', 'utf8');
+console.log ('Private key sign:', sign);
+
+const encrypted = public_key.encrypt(sign, 'base64', 'utf8');
+console.log ('Public key encryption:', encrypted);
+
+// Decrypt and verify
+const decrypted = public_key.decrypt(encrypted, 'utf8');
+console.log ('Private key decryption:', decrypted);
+
+const verify = public_key.verify(message, decrypted, 'utf8', 'base64');
+console.log ('Проверка открытого ключа:', verify);
