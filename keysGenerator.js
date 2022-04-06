82a03bea69696c675d4eb02678448774b742cddb
klltx <kostya.svetashov.01@gmail.com>
2022-04-07 03:49:41 +0400

    refactor

--- keysGenerator.js
+++ keysGenerator.js
@@ -1,18 +1,11 @@
 const NodeRSA = require('node-rsa'),
-    fs = require('fs'),
-    path = require('path');
+    fs = require('fs');
 
 function generateKeys(keyPath) {
     const key = new NodeRSA().generateKeyPair();
 
     const publicKey = key.exportKey('public'),
         privateKey = key.exportKey('private');
-
-    // const fdPublic = fs.openSync('./keys/public.pem', 'w');
-    // fs.writeSync(fdPublic, publicKey, 'utf8');
-    //
-    // const fdPrivate = fs.openSync('./keys/private.pem', 'w');
-    // fs.writeSync(fdPrivate, privateKey, 'utf8');
 
     fs.mkdir(keyPath, { recursive: true }, (err) => {
         if (err) throw err;
